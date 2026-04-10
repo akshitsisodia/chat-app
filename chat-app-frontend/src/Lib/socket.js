a51d3cae90b86ab5888f5c42c11dc678f9546bf2
Akshit Sisodiya <aakshit905@gmail.com>
2026-04-10 15:35:43 +0530

    deploy fix

--- chat-app-frontend/src/Lib/socket.js
+++ chat-app-frontend/src/Lib/socket.js
@@ -1,10 +1,9 @@
 import { io } from "socket.io-client";
-import { useAuth } from "../Context/AuthContext";
 
 let socket = null;
 
-// const api = "https://chat-app-6h3y.onrender.com"
-const api = "http://localhost:8000";
+const api = "https://chat-app-6h3y.onrender.com"
+// const api = "http://localhost:8000";
 
 export const connectSocket = () => {
 
