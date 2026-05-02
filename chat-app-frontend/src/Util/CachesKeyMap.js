5c46d08364851f804e732ca871521f9ed6757fc7
SatelliteKeyIt <akshit.sisodia@satellitekeyit.com>
2026-05-02 18:37:04 +0530

    first push

--- chat-app-frontend/src/Util/CachesKeyMap.js
+++
@@ -1,9 +0,0 @@
-const groupKeyCache = new Map();
-
-export function getCachedKey(chatId) {
-  return groupKeyCache.get(chatId);
-}
-
-export function setCachedKey(chatId, key) {
-  groupKeyCache.set(chatId, key);
-}
--- chat-app-frontend/src/webrtc/media.js
+++
@@ -1,7 +0,0 @@
-// webrtc/media.js
-export async function getMediaStream({ video }) {
-  return navigator.mediaDevices.getUserMedia({
-    video,
-    audio: true,
-  });
-}
